44a616d85927c47c6bac198603687b5fd6a4a00b
yorman <yorman40k@gmail.com>
2023-05-17 15:13:21 -0500

    wip: exporting exprees to iniciate our first server in the commonjs way

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hola mundo, ahora imprimo una suma: ', '2 + 2 = ', 2+2);
+const express = require('express');
+
+
+//Crear la app
+const app = express();
+
+//Routing
+app.get('/', function(req, res) {
+    res.send('Hola mundo en express');
+});
+
+//Definir un puerto y arrancar la app
+const port = 3000;
+
+app.listen(port, () => { 
+    console.log(`El servidor esta corriendo en el puerto ${port}`); 
+})
